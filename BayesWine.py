e063cb4cfca3033c982cc907a36cfae1a6a8d3e8
Shane Yu <shaneyu07@gmail.com>
2017-04-26 03:22:40 +0000

    get everything flexible, and the instructions are in the codes

--- BayesWine.py
+++ BayesWine.py
@@ -1,3 +1,4 @@
+# @Author: Shane Yu  @Date Created: April 25, 2017
 import numpy as np
 import pandas as pd
 from numpy.linalg import inv, det
@@ -6,20 +7,25 @@ import operator
 
 
 class BayesWine(object):
-    def __init__(self):
+    """
+    constructor:
+        Training data file's path
+
+    getDecision function:
+        Input: a line of the testing data
+        Output: a class number
+    """
+    def __init__(self, trainingDataPath):
         np.set_printoptions(precision=6, suppress=True, linewidth=1000)
         pd.set_option('display.width', 1000)
-        with open('./TrainingData_2.txt', 'r') as rf:
+        with open(trainingDataPath, 'r') as rf:
             self.pdData = pd.read_csv(rf, header=None) # [178 rows x 14 columns]
-
 
     def getCoVarianceMatrix(self, classNum):
         return self.pdData.loc[self.pdData[0]==classNum, 1:].cov()
 
-
     def getMeanVector(self, classNum):
         return self.pdData.loc[self.pdData[0]==classNum, 1:].mean()
-
 
     def getDecision(self, inputString):
         inputVector = np.array(inputString.split(',')[1:], dtype=np.float)
@@ -33,8 +39,6 @@ class BayesWine(object):
         #     print(str(key) + ' ' + str(value))
         return sorted(decisionValue.items(), key=operator.itemgetter(1))[0][0]
 
-
-
     def test(self, testString):
         # print(self.pdData.shape[0]) # 1:59 2:71 3:48 total:178
         # print(type(self.getCoVarianceMatrix(2))) # <class 'pandas.core.frame.DataFrame'>
@@ -42,8 +46,7 @@ class BayesWine(object):
         print(self.getDecision(testString))
 
 
-
 if __name__ == '__main__':
     import sys
-    obj = BayesWine()
+    obj = BayesWine('./TrainingData.txt')
     obj.test(sys.argv[1])
--- CorrectnessRatio.py
+++ CorrectnessRatio.py
@@ -1,23 +1,32 @@
+# @Author: Shane Yu  @Date Created: April 25, 2017
+"""
+system arguments:
+1) Training data path
+2) Testing data path
+"""
 from BayesWine import BayesWine
+import sys
 
-bayesObj = BayesWine()
+bayesObj = BayesWine(sys.argv[1])
 correctnessDict = {'Class1':0, 'Class2':0, 'Class3':0}
-dataCountDict = {'1':0, '2':0, '3':0}
+testingDataCountDict = {'1':0, '2':0, '3':0}
+totalDataCountDict = {1:59, 2:71, 3:48} # 每個class資料筆數
 
-with open('./TestingData_2.txt', 'r') as rf:
+
+with open(sys.argv[2], 'r') as rf:
     for line in rf:
         classNum = line.split(',')[0]
-        for key in dataCountDict.keys():
+        for key in testingDataCountDict.keys():
             if classNum == key:
-                dataCountDict[key] += 1
+                testingDataCountDict[key] += 1
         if bayesObj.getDecision(line) == int(classNum):
             correctnessDict['Class' + classNum] += 1
 
 for num in range(1, 4):
-    correctnessDict['Class' + str(num)] = correctnessDict['Class' + str(num)] / float(dataCountDict[str(num)])
+    correctnessDict['Class' + str(num)] = correctnessDict['Class' + str(num)] / float(testingDataCountDict[str(num)])
 
 for key, value in correctnessDict.items():
-    print(key + '的正確率為' + str(value))
+    print(key + ' 的正確率為' + str(value))
 
-for key, value in dataCountDict.items():
-    print(key + '的數量：' + str(value))
+for key, value in testingDataCountDict.items():
+    print('Class' + key + ' 的測試數量：' + str(value) + '  訓練數量: ' + str(totalDataCountDict[int(key)]-value) + '  訓練資料百分比: ' + str(round(100*(totalDataCountDict[int(key)]-value)/totalDataCountDict[int(key)], 2)) + ' %')
