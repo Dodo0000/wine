d70b9631ac7e34475c7780bb0ac90d9b5bf582bd
Shane Yu <shaneyu07@gmail.com>
2017-05-02 07:08:20 +0000

    comments added

--- CorrectnessRatio.py
+++ CorrectnessRatio.py
@@ -1,8 +1,7 @@
 # @Author: Shane Yu  @Date Created: April 25, 2017
 """
 system arguments:
-1) Training data path
-2) Testing data path
+1) Training data ratio (e.g. 0.5)
 """
 from BayesWine import BayesWine
 import sys
@@ -13,8 +12,7 @@ import operator
 
 totalDataCountDict = {1:59, 2:71, 3:48} # 每個class資料筆數
 correctnessDict = {'Class1':0, 'Class2':0, 'Class3':0}
-totalDataCountDict = {1:59, 2:71, 3:48} # 每個class資料筆數
-# initialize classSetList
+# 初始化classSetList
 classSetDict = dict() # {'1':set(), '2':set(), '3':set()}
 for x in range(1, 4):
     classSetDict[str(x)] = set()
@@ -23,22 +21,24 @@ with open('Data.txt', 'r') as rf:
     for line in rf:
         classSetDict[line.split(',')[0]].add(line)
 
+# 採樣訓練資料所佔比例並寫檔
 with open('TempTrainingData.txt', 'w') as wf_Train:
     for key in classSetDict.keys(): # '1', '2', '3'
         for line in random.sample(classSetDict[key], int(totalDataCountDict[int(key)]*float(sys.argv[1]))):
             wf_Train.write(line)
             classSetDict[key].remove(line)
+            # classSetDict中剩下的已是測試資料
 
+# 利用BayesWine進行分類
 bayesObj = BayesWine('TempTrainingData.txt')
-# 剩下的classSetDict已是測試資料
 for classNum in classSetDict.keys(): # '1', '2', '3'
     for line in classSetDict[classNum]:
         if bayesObj.getDecision(line) == int(classNum):
             correctnessDict['Class' + classNum] += 1
-
+# 正確率計算
 for num in range(1, 4):
     correctnessDict['Class' + str(num)] = correctnessDict['Class' + str(num)] / float(len(classSetDict[str(num)]))
-
+# 列印
 for key, value in sorted(correctnessDict.items(), key=operator.itemgetter(0)):
     print(key + ' 的正確率為' + str(value))
 
