2799486b4415b3fd3b58cfef41ace16f6485c980
Shane Yu <shaneyu07@gmail.com>
2017-05-02 03:41:27 +0000

    CorrectnessRatio.py modified to have flexible percentage of training and testing data

--- BayesWine.py
+++ BayesWine.py
@@ -9,17 +9,17 @@ import operator
 class BayesWine(object):
     """
     constructor:
-        Training data file's path
+        1) Training data file's path
 
     getDecision function:
-        Input: a line of the testing data
-        Output: a class number
+        Input: a line of the testing data (string)
+        Output: a class number (int)
     """
     def __init__(self, trainingDataPath):
         np.set_printoptions(precision=6, suppress=True, linewidth=1000)
         pd.set_option('display.width', 1000)
         with open(trainingDataPath, 'r') as rf:
-            self.pdData = pd.read_csv(rf, header=None) # [178 rows x 14 columns]
+            self.pdData = pd.read_csv(rf, header=None)
 
     def getCoVarianceMatrix(self, classNum):
         return self.pdData.loc[self.pdData[0]==classNum, 1:].cov()
@@ -35,18 +35,14 @@ class BayesWine(object):
             decisionValue[num] = (1/2) * np.dot(np.dot(np.subtract(inputVector, self.getMeanVector(num)).T, inv(self.getCoVarianceMatrix(num))), np.subtract(inputVector, self.getMeanVector(num))) \
                         + (1/2) * math.log(det(self.getCoVarianceMatrix(num))) \
                         - math.log(dataCount[num] / 178)
-        # for key, value in decisionValue.items():
-        #     print(str(key) + ' ' + str(value))
+
         return sorted(decisionValue.items(), key=operator.itemgetter(1))[0][0]
 
     def test(self, testString):
-        # print(self.pdData.shape[0]) # 1:59 2:71 3:48 total:178
-        # print(type(self.getCoVarianceMatrix(2))) # <class 'pandas.core.frame.DataFrame'>
-        # print(type(self.getMeanVector(1))) # <class 'pandas.core.series.Series'>
         print(self.getDecision(testString))
 
 
 if __name__ == '__main__':
     import sys
-    obj = BayesWine('./TrainingData.txt')
+    obj = BayesWine('./Data.txt')
     obj.test(sys.argv[1])
--- CorrectnessRatio.py
+++ CorrectnessRatio.py
@@ -6,27 +6,43 @@ system arguments:
 """
 from BayesWine import BayesWine
 import sys
+import random
+import subprocess
+import operator
 
-bayesObj = BayesWine(sys.argv[1])
-correctnessDict = {'Class1':0, 'Class2':0, 'Class3':0}
-testingDataCountDict = {'1':0, '2':0, '3':0}
+
 totalDataCountDict = {1:59, 2:71, 3:48} # 每個class資料筆數
+correctnessDict = {'Class1':0, 'Class2':0, 'Class3':0}
+totalDataCountDict = {1:59, 2:71, 3:48} # 每個class資料筆數
+# initialize classSetList
+classSetDict = dict() # {'1':set(), '2':set(), '3':set()}
+for x in range(1, 4):
+    classSetDict[str(x)] = set()
 
-
-with open(sys.argv[2], 'r') as rf:
+with open('Data.txt', 'r') as rf:
     for line in rf:
-        classNum = line.split(',')[0]
-        for key in testingDataCountDict.keys():
-            if classNum == key:
-                testingDataCountDict[key] += 1
+        classSetDict[line.split(',')[0]].add(line)
+
+with open('TempTrainingData.txt', 'w') as wf_Train:
+    for key in classSetDict.keys(): # '1', '2', '3'
+        for line in random.sample(classSetDict[key], int(totalDataCountDict[int(key)]*float(sys.argv[1]))):
+            wf_Train.write(line)
+            classSetDict[key].remove(line)
+
+bayesObj = BayesWine('TempTrainingData.txt')
+# 剩下的classSetDict已是測試資料
+for classNum in classSetDict.keys(): # '1', '2', '3'
+    for line in classSetDict[classNum]:
         if bayesObj.getDecision(line) == int(classNum):
             correctnessDict['Class' + classNum] += 1
 
 for num in range(1, 4):
-    correctnessDict['Class' + str(num)] = correctnessDict['Class' + str(num)] / float(testingDataCountDict[str(num)])
+    correctnessDict['Class' + str(num)] = correctnessDict['Class' + str(num)] / float(len(classSetDict[str(num)]))
 
-for key, value in correctnessDict.items():
+for key, value in sorted(correctnessDict.items(), key=operator.itemgetter(0)):
     print(key + ' 的正確率為' + str(value))
 
-for key, value in testingDataCountDict.items():
-    print('Class' + key + ' 的測試數量：' + str(value) + '  訓練數量: ' + str(totalDataCountDict[int(key)]-value) + '  訓練資料百分比: ' + str(round(100*(totalDataCountDict[int(key)]-value)/totalDataCountDict[int(key)], 2)) + ' %')
+for key, value in sorted(classSetDict.items(), key=operator.itemgetter(0)):
+    print('Class' + key + ' 的測試數量：' + str(len(value)) + '  訓練數量: ' + str(totalDataCountDict[int(key)]-len(value)) + '  訓練資料百分比: ' + str(round(100*(totalDataCountDict[int(key)]-len(value))/totalDataCountDict[int(key)], 2)) + ' %')
+
+subprocess.call(['rm', 'TempTrainingData.txt'])
