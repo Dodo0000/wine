d0cf7289d01a39c14f6ab33b029290f48b485a65
Shane Yu <shaneyu07@gmail.com>
2017-06-08 13:11:06 +0000

    Logistic regression done

---
+++ CorrectnessRatio.py
@@ -0,0 +1,66 @@
+# @Author: Shane Yu  @Date Created: April 25, 2017
+"""
+system arguments:
+1) Training data ratio (e.g. 0.5)
+"""
+from BayesWine import NaiveBayes, MinDistance
+import sys
+import random
+import subprocess
+import operator
+
+print('---------------------')
+print('Bayes Classifier :')
+totalDataCountDict = {1:59, 2:71, 3:48} # 每個class資料筆數
+correctnessDict = {'Class1':0, 'Class2':0, 'Class3':0}
+# 初始化classSetList
+classSetDict = dict() # {'1':set(), '2':set(), '3':set()}
+for x in range(1, 4):
+    classSetDict[str(x)] = set()
+
+with open('Data.txt', 'r') as rf:
+    for line in rf:
+        classSetDict[line.split(',')[0]].add(line)
+
+# 採樣訓練資料所佔比例並寫檔
+with open('TempTrainingData.txt', 'w') as wf_Train:
+    for key in classSetDict.keys(): # '1', '2', '3'
+        for line in random.sample(classSetDict[key], int(totalDataCountDict[int(key)]*float(sys.argv[1]))):
+            wf_Train.write(line)
+            classSetDict[key].remove(line)
+            # classSetDict中剩下的已是測試資料
+
+# 利用NaiveBayes進行分類
+bayesObj = NaiveBayes('TempTrainingData.txt')
+for classNum in classSetDict.keys(): # '1', '2', '3'
+    for line in classSetDict[classNum]:
+        if bayesObj.getDecision(line) == int(classNum):
+            correctnessDict['Class' + classNum] += 1
+# 正確率計算
+for num in range(1, 4):
+    correctnessDict['Class' + str(num)] = correctnessDict['Class' + str(num)] / float(len(classSetDict[str(num)]))
+# 列印
+for key, value in sorted(correctnessDict.items(), key=operator.itemgetter(0)):
+    print(key + ' 的正確率為' + str(value))
+ 
+print('---------------------')
+print('minimum distances :')
+correctnessDict = {'Class1':0, 'Class2':0, 'Class3':0}
+# 利用MinDistance進行分類
+minDistanceObj = MinDistance('TempTrainingData.txt')
+for classNum in classSetDict.keys(): # '1', '2', '3'
+    for line in classSetDict[classNum]:
+        if minDistanceObj.getClassification(line) == int(classNum):
+            correctnessDict['Class' + classNum] += 1
+# 正確率計算
+for num in range(1, 4):
+    correctnessDict['Class' + str(num)] = correctnessDict['Class' + str(num)] / float(len(classSetDict[str(num)]))
+# 列印
+for key, value in sorted(correctnessDict.items(), key=operator.itemgetter(0)):
+    print(key + ' 的正確率為' + str(value))
+
+print('---------------------')
+for key, value in sorted(classSetDict.items(), key=operator.itemgetter(0)):
+    print('Class' + key + ' 的測試數量：' + str(len(value)) + '  訓練數量: ' + str(totalDataCountDict[int(key)]-len(value)) + '  訓練資料百分比: ' + str(round(100*(totalDataCountDict[int(key)]-len(value))/totalDataCountDict[int(key)], 2)) + ' %')
+
+subprocess.call(['rm', 'TempTrainingData.txt'])
--- Logistic-Regression.py
+++ Logistic-Regression.py
@@ -1,4 +1,7 @@
 # Author: Shane Yu  Data Created: June 7, 2017
+import matplotlib
+# matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -13,6 +16,8 @@ class LogisticRegression(object):
         data = pd.read_csv(self.dataPath, header=None).values
         nestedData = list()
         for line in data:
+            if line[0] == 3: continue
+            line[0] -= 1
             # print(line)
             tempList = list()
             dataList = list()
@@ -21,18 +26,6 @@ class LogisticRegression(object):
                 dataList.append(item)
             tempList.append(dataList)
             tempList.append(line[0])
-            nestedData.append(tempList)
-        nestedData = np.array(nestedData)
-        # print(nestedData)
-        return nestedData
-
-    def dataPreprocessing_forTempData(self):
-        data = pd.read_csv(self.dataPath, header=None).values
-        nestedData = list()
-        for line in data:
-            tempList = list()
-            tempList.append(line[:3])
-            tempList.append(line[3])
             nestedData.append(tempList)
         nestedData = np.array(nestedData)
         # print(nestedData)
@@ -63,38 +56,108 @@ class LogisticRegression(object):
 
     # 演算法實作
     def logistic(self, dataset):
-
-        w = np.random.rand(14) #初始設定w為(0, 0,..., 0)
         # w = np.zeros(3) #初始設定w為(0, 0,..., 0)
-
-        limit = 1000 # 更新100次後停下
-
-        eta = 0.1 # 更新幅度
+        w = np.random.rand(14) #初始設定w為(0, 0,..., 0)
+        iterations = 100 # 更新100次後停下
+        learning_rate = 0.01 # 更新幅度
 
         costs = [] # 紀錄每次更新權重後新的cost是多少
 
-        for i in range(limit):
+        for i in range(iterations):
             current_cost = self.cost(dataset, w) # 當前訓練iteration的cost
             print('current_cost=' + str(current_cost))
             costs.append(current_cost)
-            w = w - eta * self.gradient(dataset, w)  # w更新的方式: w - learning_rate * 梯度 (例如第一輪，試算出w在w=[0 0 0]時對L的偏微結果)
-            eta *= 0.95 # Learning Rate，逐步遞減
+            w = w - learning_rate * self.gradient(dataset, w)  # w更新的方式: w - learning_rate * 梯度 (例如第一輪，試算出w在w=[0 0 0]時對L的偏微結果)
+            # print(w)
+            learning_rate *= 0.95 # Learning Rate，逐步遞減
 
         # 畫出cost的變化曲線，他應該要是不斷遞減 才是正確
-
-        plt.plot(range(limit), costs)
-        plt.show()
+        plt.plot(range(iterations), costs)
+        # plt.show()
+        plt.savefig('Plot.png', format='png')
         return w
 
-    def activate(self):
+    def getWeights(self):
         data = self.dataPreprocessing()
-        # data = self.dataPreprocessing_forTempData()
+        print(data)
+        w = self.logistic(data)
+        print(w)
+
+
+class TempData(object):
+    def __init__(self, dataPath):
+        self.dataPath = dataPath
+        np.set_printoptions(precision=6, suppress=True, linewidth=100000)
+
+    def dataPreprocessing(self):
+        data = pd.read_csv(self.dataPath, header=None).values
+        nestedData = list()
+        for line in data:
+            tempList = list()
+            tempList.append(line[:3])
+            tempList.append(line[3])
+            nestedData.append(tempList)
+        nestedData = np.array(nestedData)
+        # print(nestedData)
+        return nestedData
+
+    # sigmoid function
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+    
+    # 計算平均梯度
+    def gradient(self, dataset, w): # 偏微分方向
+        g = np.zeros(len(w))
+        for x, y in dataset: # x是訓練資料 y是該筆資料屬於的類別
+            x = np.array(x)
+            error = self.sigmoid(w.T.dot(x))
+            g += (error - y) * x
+        return g / len(dataset)
+
+    # 計算現在的權重的錯誤有多少
+    def cost(self, dataset, w): # Loss funcion
+        total_cost = 0
+        for x, y in dataset:
+            x = np.array(x) # 訓練資料x
+            error = self.sigmoid(w.T.dot(x))
+            total_cost += abs(y - error)
+        return total_cost
+
+    # 演算法實作
+    def logistic(self, dataset):
+        # w = np.zeros(3) #初始設定w為(0, 0,..., 0)
+        w = np.random.rand(3) #初始設定w為(0, 0,..., 0)
+        iterations = 100 # 更新100次後停下
+        learning_rate = 0.5 # 更新幅度
+
+        costs = [] # 紀錄每次更新權重後新的cost是多少
+
+        for i in range(iterations):
+            current_cost = self.cost(dataset, w) # 當前訓練iteration的cost
+            print('current_cost=' + str(current_cost))
+            costs.append(current_cost)
+            w = w - learning_rate * self.gradient(dataset, w)  # w更新的方式: w - learning_rate * 梯度 (例如第一輪，試算出w在w=[0 0 0]時對L的偏微結果)
+            # print(w)
+            learning_rate *= 0.95 # Learning Rate，逐步遞減
+
+        # 畫出cost的變化曲線，他應該要是不斷遞減 才是正確
+        plt.plot(range(iterations), costs)
+        # plt.show()
+        plt.savefig('tempDataPlot.png', format='png')
+        return w
+
+    def getWeights(self):
+        data = self.dataPreprocessing()
         # print(data)
         w = self.logistic(data)
         print(w)
 
 
 if __name__ == '__main__':
-    # obj = LogisticRegression('testData.csv')
-    obj = LogisticRegression('data.csv')
-    obj.activate()
+    import sys
+    if sys.argv[1] == '1':
+        obj = LogisticRegression('data.csv')
+    else:
+        obj = TempData('testData.csv')
+    obj.getWeights()
+    
