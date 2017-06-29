b6b38fde6f8433b30a025aeb62891c6aa4e9ad41
Shane Yu <shaneyu07@gmail.com>
2017-06-29 02:25:39 +0000

    keras model in process

--- DNN/dnn.py
+++ DNN/dnn.py
@@ -1,4 +1,6 @@
-# import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from sklearn.model_selection import train_test_split
 import pandas as pd
 from os.path import join
 
@@ -7,7 +9,10 @@ class dnn(object):
         self.data = pd.read_csv(data_path, header=None)
 
     def function(self):
-        print(self.data)
+        # print(self.data)
+        X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.4, random_state=0)
+        # model = Sequential()
+        # model.add(Dense(32, input_dim=784))
 
 if __name__ == '__main__':
     dnn_obj = dnn()
